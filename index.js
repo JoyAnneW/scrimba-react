ea92b5ab5e97e3dc5c1aeee244a2ba5c93e73f41
JoyAnneW <joyawilliams@gmail.com>
2022-01-10 21:53:13 +0100

    first lines of react. added title and list to static page

---
+++ index.js
@@ -0,0 +1,15 @@
+ReactDOM.render(
+	<h1>Fun facts about React</h1>,
+	document.querySelector("#root")
+);
+
+ReactDOM.render(
+	<ul>
+		<li>Was first released in 2013</li>
+		<li>Was originally created by Jordan Walke</li>
+		<li>Has well over 100K stars on GitHub</li>
+		<li>Is maintained by Facebook</li>
+		<li>Powers thousands of enterprise apps, including mobile apps</li>
+	</ul>,
+	document.querySelector("#list")
+);
