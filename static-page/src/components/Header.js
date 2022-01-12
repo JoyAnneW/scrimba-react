6b489f7319d517870ea09e700371bfbb98bbd03f
JoyAnneW <joyawilliams@gmail.com>
2022-01-12 03:31:52 +0100

    switched to Create React App config, made Header and Main components

---
+++ static-page/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+	return (
+		<header>
+			<nav>
+				<img src="./react-logo.png" alt="react logo" width="40px" />
+				<title>ReactFacts</title>
+				<span>React Course - Project 1</span>
+			</nav>
+		</header>
+	);
+}
---
+++ static-page/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
+
+function App() {
+	return (
+		<div>
+			<Header />
+			<Main />
+		</div>
+	);
+}
+
+ReactDOM.render(<App />, document.querySelector("#root"));
