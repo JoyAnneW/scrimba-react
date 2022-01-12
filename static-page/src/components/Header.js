51876cc0b20623f6c775bbaff8721f2ad86e290b
JoyAnneW <joyawilliams@gmail.com>
2022-01-12 10:06:26 +0100

    added logo

--- static-page/src/components/Header.js
+++ static-page/src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import reactLogo from "../images/react-logo.png";
 
 export default function Header() {
 	return (
 		<header>
 			<nav>
-				<img src="./react-logo.png" alt="react logo" width="40px" />
+				<img src={reactLogo} alt="react logo" width="40px" />
 				<title>ReactFacts</title>
 				<span>React Course - Project 1</span>
 			</nav>
